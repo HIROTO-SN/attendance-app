11fb1b15bad82b7eaa4efe085e02bbafb159f108
Hiroto N <hiroto4455656@gmail.com>
2026-01-08 05:17:09 +0900

    shift filament modified

--- app/Filament/Resources/Shifts/Schemas/ShiftForm.php
+++ app/Filament/Resources/Shifts/Schemas/ShiftForm.php
@@ -2,28 +2,41 @@
 
 namespace App\Filament\Resources\Shifts\Schemas;
 
-use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TimePicker;
 use Filament\Schemas\Schema;
 
-class ShiftForm
-{
-    public static function configure(Schema $schema): Schema
-    {
+class ShiftForm {
+    public static function configure( Schema $schema ): Schema {
         return $schema
-            ->components([
-                TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
-                DatePicker::make('shift_date')
-                    ->required(),
-                TimePicker::make('start_time'),
-                TimePicker::make('end_time'),
-                TextInput::make('break_minutes')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-            ]);
+        ->components( [
+            Select::make( 'user_id' )
+            ->required()
+            ->searchable()
+            ->preload()
+            ->relationship( 'user', 'name' ),
+            TextInput::make( 'work_type' )
+            ->required(),
+            TextInput::make( 'daily_work_minutes' )
+            ->required()
+            ->numeric(),
+            TextInput::make( 'break_minutes' )
+            ->required()
+            ->numeric()
+            ->default( 60 ),
+            TimePicker::make( 'standard_start_time' )
+            ->seconds( false )
+            ->required(),
+            TimePicker::make( 'standard_end_time' )
+            ->seconds( false )
+            ->required(),
+            TimePicker::make( 'core_start_time' )
+            ->seconds( false )
+            ->required(),
+            TimePicker::make( 'core_end_time' )
+            ->seconds( false )
+            ->required(),
+        ] );
     }
 }
